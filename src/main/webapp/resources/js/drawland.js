2735bd4b28e9d3f56013cb8cee389edf16ab9acd
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2019-12-02 21:20:50 +0700

    update search by gg map

--- src/main/webapp/resources/js/drawland.js
+++ src/main/webapp/resources/js/drawland.js
@@ -6,10 +6,10 @@
 var map;
 var selectedMarkers = [];
 console.log('ok');
-function initMap(){
+function initMap() {
     //FPT 
-    var latitude = 21.012633; 
-    var longitude = 105.527423; 
+    var latitude = 21.012633;
+    var longitude = 105.527423;
 
     var myLatLng = {lat: latitude, lng: longitude};
 
@@ -18,7 +18,7 @@ function initMap(){
         zoom: 14,
         disableDoubleClickZoom: true, // disable the default map zoom on double click
     });
-    
+
     // Create new marker on single click event on the map
     google.maps.event.addListener(map, 'click', function (event) {
         var marker = new google.maps.Marker({
@@ -34,6 +34,62 @@ function initMap(){
 //        
 //        listCoordinates.push([event.latLng.lat(),event.latLng.lng()]);
 //        document.getElementById('form:json').value = JSON.stringify(listCoordinates);
+    });
+    var input = document.getElementById('input-gg-map');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+//            markers.push(new google.maps.Marker({
+//                map: map,
+//                icon: icon,
+//                title: place.name,
+//                position: place.geometry.location
+//            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
     });
 }
 

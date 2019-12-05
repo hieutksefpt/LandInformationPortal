f2226ca01f5c1c96161211540de4a99637869287
Phong <trantuanphong.ttt@users.noreply.github.com>
2019-12-05 23:02:10 +0700

    update ViewRoad based on DrawLand

--- src/main/java/capstone/rep/realestateportal/bean/ViewRoadBean.java
+++ src/main/java/capstone/rep/realestateportal/bean/ViewRoadBean.java
@@ -5,12 +5,15 @@
  */
 package capstone.rep.realestateportal.bean;
 
+import capstone.rep.realestateportal.entity.Land;
 import capstone.rep.realestateportal.entity.Road;
 import capstone.rep.realestateportal.service.CommonService;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.primefaces.json.JSONObject;
 
 /**
  *
@@ -19,18 +22,94 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class ViewRoadBean {
-    private String roadName;
-    private String areaNearName;
+
+    private String roadId;
+    private String geoJSON;
+
+    private String roadSegmentName;
+    private String roadSegmentMeanPrice;
+    private String roadSegmentPredictPrice;
+    private String roadSegmentMinPrice;
+    private String roadSegmentMaxPrice;
     
     @PostConstruct
-    public void init(){
-        
+    public void init() {
+        //sample data
+        roadSegmentName = "Thử nghiệm 1";
+        roadSegmentMeanPrice = "150.000.000/m2";
+        roadSegmentPredictPrice = "167.000.000/m2";
+        roadSegmentMinPrice = "100.000.000/m2";
+        roadSegmentMaxPrice = "200.000.000/m2";
+    }
+
+    public List<Road> listRoadByHint() {
+        String hintLowerCase = (roadId + "").toLowerCase();
+        CommonService commonService = new CommonService();
+        List<Road> listRoadByHint = commonService.getRoadByHint(hintLowerCase);
+        return listRoadByHint.stream().collect(Collectors.toList());
+    }
+
+    public void changeRoadViewById() {
+        CommonService commonService = new CommonService();
+        List<Land> listLandNearroad = commonService.getLandNearByroadId(roadId);
+        JSONObject jsonObject = commonService.createGeoJson(listLandNearroad);
+        geoJSON = jsonObject.toString();
+    }
+
+    public String getRoadId() {
+        return roadId;
+    }
+
+    public void setRoadId(String roadId) {
+        this.roadId = roadId;
+    }
+
+    public String getGeoJSON() {
+        return geoJSON;
+    }
+
+    public void setGeoJSON(String geoJSON) {
+        this.geoJSON = geoJSON;
+    }
+
+    public String getRoadSegmentName() {
+        return roadSegmentName;
+    }
+
+    public void setRoadSegmentName(String roadSegmentName) {
+        this.roadSegmentName = roadSegmentName;
+    }
+
+    public String getRoadSegmentMeanPrice() {
+        return roadSegmentMeanPrice;
+    }
+
+    public void setRoadSegmentMeanPrice(String roadSegmentMeanPrice) {
+        this.roadSegmentMeanPrice = roadSegmentMeanPrice;
+    }
+
+    public String getRoadSegmentPredictPrice() {
+        return roadSegmentPredictPrice;
+    }
+
+    public void setRoadSegmentPredictPrice(String roadSegmentPredictPrice) {
+        this.roadSegmentPredictPrice = roadSegmentPredictPrice;
+    }
+
+    public String getRoadSegmentMinPrice() {
+        return roadSegmentMinPrice;
+    }
+
+    public void setRoadSegmentMinPrice(String roadSegmentMinPrice) {
+        this.roadSegmentMinPrice = roadSegmentMinPrice;
+    }
+
+    public String getRoadSegmentMaxPrice() {
+        return roadSegmentMaxPrice;
+    }
+
+    public void setRoadSegmentMaxPrice(String roadSegmentMaxPrice) {
+        this.roadSegmentMaxPrice = roadSegmentMaxPrice;
     }
     
-    public List<Road> getRoadByHint(String hint){
-        String hintLowerCase = hint.toLowerCase();
-        CommonService commonService = new CommonService();
-        List<Road> listroadByHint = commonService.getRoadByHint(hintLowerCase);
-        return listroadByHint;
-    }
 }

ff73fea853b4462553b26414a2be9d4bbcc0e72e
haonase06089 <40634213+haonase06089@users.noreply.github.com>
2019-12-09 14:47:03 +0700

    commit connection file from bo hao dep trai

--- src/main/java/capstone/rep/realestateportal/connection/Connection.java
+++ src/main/java/capstone/rep/realestateportal/connection/Connection.java
@@ -5,10 +5,33 @@
  */
 package capstone.rep.realestateportal.connection;
 
+import java.sql.DriverManager;
+
 /**
  *
  * @author Phong
  */
 public class Connection {
-    
+    public static String username = "";
+    public static String password = "";
+    public static String url = "";
+    public static java.sql.Connection conn = null;
+
+    public static Connection dBContext = new Connection();
+
+    public Connection() {
+        initConfig();
+    }
+
+    public static void initConfig() {
+        url = "jdbc:sqlserver://localhost:1434;databaseName=DigitalNew";
+        username = "sa";
+        password = "123456";
+    }
+
+    public java.sql.Connection getConnection() throws Exception {
+        //  initConfig();
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, username, password);
+    }
 }

3fa91395219a7739bba4ee49e7cfc3903f74cb17
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-01-07 22:24:25 +0700

    update js init map

---
+++ src/main/webapp/resources/js/showlayer.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var map;
+var selectedMarkers = [];
+function initMap() {
+    //FPT 
+    var latitude = 21.012633;
+    var longitude = 105.527423;
+
+    var myLatLng = {lat: latitude, lng: longitude};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14,
+        mapTypeId: 'roadmap',
+        clickableIcons: false,
+        disableDoubleClickZoom: true, // disable the default map zoom on double click
+    });
+}
+
+

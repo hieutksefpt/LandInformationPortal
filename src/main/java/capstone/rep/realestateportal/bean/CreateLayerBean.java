1cd95cac94461e1453edcd7ed4eb4fa266d6a3ea
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-01-08 22:40:16 +0700

    Init createlayer screen

--- src/main/java/capstone/rep/realestateportal/bean/CreateLayerBean.java
+++ src/main/java/capstone/rep/realestateportal/bean/CreateLayerBean.java
@@ -1,7 +1,14 @@
 package capstone.rep.realestateportal.bean;
 
+import capstone.rep.realestateportal.entity.Road;
+import capstone.rep.realestateportal.entity.RoadSegment;
+import capstone.rep.realestateportal.service.CommonService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.json.JSONObject;
 
 /**
  * @dateCreate 07/01/2020
@@ -11,5 +18,60 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class CreateLayerBean {
+
+    private String roadId;
+    private String gjsonRoad;
+    private String jsonCoordinateSubmit;
+    
+    //private List<RoadSegment> listRoadSegment;
+    
+    public List<Road> listRoadByHint(String hint) {
+        if (hint == null) {
+            hint = "";
+        }
+        String hintLowerCase = (hint + "").toLowerCase();
+        CommonService commonService = new CommonService();
+        List<Road> listRoadByHint = commonService.getRoadByHint(hintLowerCase);
+        return listRoadByHint.stream().collect(Collectors.toList());
+    }
+    
+    public ArrayList<RoadSegment> getListRoadSegmentOfRoad() {
+        ArrayList sample = new ArrayList();
+        sample.add(new RoadSegment(0, "Test 1", null, null));
+        return sample;
+    }
+    
+    public void changeRoadViewById() {
+        CommonService commonService = new CommonService();
+        //Using common service get road segment by road id
+        //listRoadSegment = commonService.getListRoadSegmentByRoadId(roadId);
+        //JSONObject jsonObject = commonService.createLineGeoJSON(listRoadSegment);
+        //geoJSON = jsonObject.toString();
+    }
+
+    public String getJsonCoordinateSubmit() {
+        return jsonCoordinateSubmit;
+    }
+
+    public void setJsonCoordinateSubmit(String jsonCoordinateSubmit) {
+        this.jsonCoordinateSubmit = jsonCoordinateSubmit;
+    }
+
+    public String getGjsonRoad() {
+        return gjsonRoad;
+    }
+
+    public void setGjsonRoad(String gjsonRoad) {
+        this.gjsonRoad = gjsonRoad;
+    }
+
+
+    public String getRoadId() {
+        return roadId;
+    }
+
+    public void setRoadId(String roadId) {
+        this.roadId = roadId;
+    }
 
 }
--- src/main/webapp/resources/js/createlayer.js
+++ src/main/webapp/resources/js/createlayer.js
@@ -3,8 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* global x, google */
+
 var map;
 var selectedMarkers = [];
+var coordinateMarkers = [];
+var shape;
 function initMap() {
     //FPT 
     var latitude = 21.012633;
@@ -17,8 +21,58 @@ function initMap() {
         zoom: 14,
         mapTypeId: 'roadmap',
         clickableIcons: false,
-        disableDoubleClickZoom: true, // disable the default map zoom on double click
+        disableDoubleClickZoom: true // disable the default map zoom on double click
+    });
+    
+    // Create new marker on single click event on the map
+    google.maps.event.addListener(map, 'click', function (event) {
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: event.latLng.lat() + ', ' + event.latLng.lng()
+        });
+
+        selectedMarkers.push(marker);
+        coordinateMarkers = selectedMarkers.map(x => ({
+            lat: parseFloat(x.getPosition().lat()),
+            lng: parseFloat(x.getPosition().lng())
+        }));
     });
 }
 
+function clearInput(){
+    removeMarkers();
+    shape.setMap(null);
+}
 
+function removeMarkers(){
+    for (var i=0; i < selectedMarkers.length; i++){
+        selectedMarkers[i].setMap(null);
+    }
+    selectedMarkers = [];
+}
+
+function drawPolygon(){
+    var path = [];
+    for (var i=0; i< selectedMarkers.length; i++){
+        path.push({
+           lat: parseFloat(selectedMarkers[i].getPosition().lat()),
+           lng: parseFloat(selectedMarkers[i].getPosition().lng())
+        });
+    }
+    shape = new google.maps.Polygon({
+        paths: path,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+    shape.setMap(map);
+    $('#form-submit\\:listCoordinate').val(JSON.stringify(coordinateMarkers));
+}
+
+function drawRoad() {
+    var json = $('#geojson').val();
+    dataLayer = map.data.addGeoJson(JSON.parse(json));
+}

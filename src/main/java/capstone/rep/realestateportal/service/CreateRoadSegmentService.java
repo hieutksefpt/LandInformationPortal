618a06c92a58d0e6ce537ca13bcbcccfffcbe3b1
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-01-08 23:21:55 +0700

    done init

--- src/main/java/capstone/rep/realestateportal/service/CreateRoadSegmentService.java
+++ src/main/java/capstone/rep/realestateportal/service/CreateRoadSegmentService.java
@@ -1,9 +1,15 @@
 package capstone.rep.realestateportal.service;
+
+import capstone.rep.realestateportal.entity.Road;
+import capstone.rep.realestateportal.entity.RoadSegment;
+
 /**
  * @dateCreate 07/01/2020
  * @author tuans
  * @description Service for CreateRoadSegment.java
  */
 public class CreateRoadSegmentService {
-
+	public void insertNewRoadSegmentByRoad(RoadSegment roadSegment, Road road) {
+		
+	}
 }

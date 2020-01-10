5cab71633e031e0d67d0f055fb06caf5f1962c76
hieutkse <hieutksefpt@gmail.com>
2020-01-10 09:47:16 +0700

    commit Service ShowLayerService

--- src/main/java/capstone/rep/realestateportal/service/ShowLayerService.java
+++ src/main/java/capstone/rep/realestateportal/service/ShowLayerService.java
@@ -1,10 +1,35 @@
 package capstone.rep.realestateportal.service;
 
+import capstone.rep.realestateportal.common.Checking;
+import capstone.rep.realestateportal.dao.LayerDAO;
+import capstone.rep.realestateportal.dao.ReoDAO;
+import capstone.rep.realestateportal.entity.LandNearRoad;
+import capstone.rep.realestateportal.entity.Layer;
+import capstone.rep.realestateportal.entity.RealEstateObject;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @dateCreate 07/01/2020
  * @author tuans
  * @description Service for ShowLayerBean.java
+ * @dateUpdate 10/01/2020
+ * @UpdateAuthor Hieu
  */
 public class ShowLayerService {
 
+    // function to get List Layer By Road Name on map
+    public List<Layer> getListLayerByRoad(String roadName) {
+        LayerDAO layerDAO = new LayerDAO();
+        List<Layer> listLayer = new ArrayList<>();
+        try {
+            List<Layer> listLayerByRoad = layerDAO.getListLayerByRoad_DBNew(roadName);
+            for (Layer layer : listLayerByRoad) {
+                listLayer.add(layer);
+            }
+        } catch (Exception e) {
+            System.out.print(e.getStackTrace());
+        }
+        return listLayer;
+    }
 }

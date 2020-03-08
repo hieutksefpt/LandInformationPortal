115157101123c991644ff848b09390daf1d55ff0
haonase06089 <40634213+haonase06089@users.noreply.github.com>
2020-03-08 21:24:03 +0700

    hoàn thành khung của màn manageRealEstateFeature (front-end, not back-end)

--- src/main/webapp/resources/js/manageRealEstateFeature.js
+++ src/main/webapp/resources/js/manageRealEstateFeature.js
@@ -1,5 +1,24 @@
 function openEditPopup(){
-	$('#exampleModalCenter').modal('show');
+    $('#editPopup').modal('show');
 }
 
+function openDeletePopup(){
+    $('#deletePopup').modal('show');
+}
+
+function openEditPopup2(){
+    $('#editPopup2').modal('show');
+}
+
+function openDeletePopup2(){
+    $('#deletePopup2').modal('show');
+}
+
+function openAddFeaturePopup(){
+    if($('#view\\:landFeature').attr('aria-hidden')==="false"){
+        $('#addLandFeaturePopup').modal('show');
+    }else if($('#view\\:houseFeature').attr('aria-hidden')==="false"){
+        $('#addHouseFeaturePopup').modal('show');
+    }
+}
 

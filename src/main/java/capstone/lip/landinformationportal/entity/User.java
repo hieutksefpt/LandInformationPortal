4306123a9af647dc77fbc222f3ffd8d695121cac
hieutkse <hieutksefpt@gmail.com>
2020-03-08 16:41:06 +0700

    commit Dev_HouseLand

---
+++ src/main/java/capstone/lip/landinformationportal/entity/User.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.lip.landinformationportal.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Admin
+ */
+@Entity
+@Table(name = "User")
+public class User extends AuditAbstract implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserID")
+    private Long userId;
+    @Column(name = "UserName")
+    private String userName;
+    @Column(name = "Password")
+    private String password;
+    @Column(name = "FullName")
+    private String fullName;
+    @Column(name = "Role")
+    private String role;
+    @Column(name = "Email")
+    private String email;
+    @Column(name = "Phone")
+    private String phone;
+    
+    public User() {
+    }
+
+    public User(Long userId, String userName, String password, String fullName, String role, String email, String phone) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+}
+

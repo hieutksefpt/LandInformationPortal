64346b0fecd14562bab682a28eee4ac4de50e294
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-03-08 23:31:32 +0700

    commit first test GUI ManageGeoInfo

--- src/test/java/capstone/lip/landinformationportal/screen/ManageGeoInfoTest.java
+++ src/test/java/capstone/lip/landinformationportal/screen/ManageGeoInfoTest.java
@@ -5,7 +5,7 @@
  */
 package capstone.lip.landinformationportal.screen;
 
-import capstone.lip.landinformationportal.common.ChromeDevice;
+import capstone.lip.landinformationportal.common.GUITest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,57 +18,178 @@ import org.openqa.selenium.By;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ManageGeoInfoTest extends ChromeDevice {
+public class ManageGeoInfoTest extends GUITest {
     
     private void openSite() throws InterruptedException {
-        driver.get("http://localhost:8080/managegeoinfo.xhtml");
-        Thread.currentThread().sleep(1000);
+        driver.get("http://localhost:8080/admin/managegeoinfo.xhtml");
+        Thread.currentThread().sleep(200);
     }
     
     private void checkCommonItem() {
-        assertFalse(driver.findElements(
-                By.xpath("//*[contains(@id,'searchbox-Address')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//select[contains(@id,'cbb-Province')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//select[contains(@id,'cbb-Street')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//select[contains(@id,'cbb-District')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//select[contains(@id,'cbb-SegmentOfStreet')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//select[contains(@id,'cbb-IpgType')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//input[contains(@id,'txtInput-Name')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//input[contains(@id,'txtInput-Coordinate')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//button[contains(@id,'btn-AddNew')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//button[contains(@id,'btn-Save')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//button[contains(@id,'btn-Delete')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//input[contains(@id,'txtInput-CoordinateContribute')]"))
-                .isEmpty());
-        assertFalse(driver.findElements(
-                By.xpath("//input[contains(@id,'txtInput-Name')]"))
-                .isEmpty());
+        checkAdminLeftBarItem();
+        
+        checkExistItem("//input[contains(@id,'searchbox-Address')]");
+        checkExistItem("//select[contains(@id,'cbb-Province')]");
+        checkExistItem("//select[contains(@id,'cbb-District')]");
+        checkExistItem("//select[contains(@id,'cbb-Street')]");
+        checkExistItem("//select[contains(@id,'cbb-SegmentOfStreet')]");
+        checkExistItem("//select[contains(@id,'cbb-IpgType')]");
+        checkExistItem("//input[contains(@id,'txtInput-Name')]");
+        checkExistItem("//input[contains(@id,'txtInput-Coordinate')]");
+        checkExistItem("//button[contains(@id,'btn-AddNew')]");
+        checkExistItem("//button[contains(@id,'btn-Save')]");
+        checkExistItem("//button[contains(@id,'btn-Delete')]");
+        checkExistItem("//button[contains(@id,'btn-Clear')]");
+        checkExistItem("//input[contains(@id,'txtInput-CoordinateContribute')]");
+        checkExistItem("//button[contains(@id,'btn-ClearSegment')]");
     }
     
+    private void checkSegmentItem() {
+        checkExistItem("//input[contains(@id,'txtInput-SegmentName')]");
+        checkExistItem("//input[contains(@id,'txtInput-CoordinateContribute')]");
+        checkExistItem("//button[contains(@id,'btn-SaveSegmentStreet')]");
+    }
+    
+    /*
+    * UC001: List province
+    */
     @Test
-    public void testExistedItem_UC001() throws Exception {
+    public void GUI_UC001() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC002: Add new province
+    */
+    @Test
+    public void GUI_UC002() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC003: Delete province
+    */
+    @Test
+    public void GUI_UC003() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC004: Update province
+    */
+    @Test
+    public void GUI_UC004() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC005: List district by province
+    */
+    @Test
+    public void GUI_UC005() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC006: Add new district
+    */
+    @Test
+    public void GUI_UC006() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC007: Delete district
+    */
+    @Test
+    public void GUI_UC007() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC008: Update district
+    */
+    @Test
+    public void GUI_UC008() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC009: List street by district
+    */
+    @Test
+    public void GUI_UC009() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC010: Add new street
+    */
+    @Test
+    public void GUI_UC010() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC011: Delete street
+    */
+    @Test
+    public void GUI_UC011() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC012: Update street
+    */
+    @Test
+    public void GUI_UC012() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC013: List segment by street
+    */
+    @Test
+    public void GUI_UC013() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC014: Add new segment
+    */
+    @Test
+    public void GUI_UC014() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC015: Delete segment
+    */
+    @Test
+    public void GUI_UC015() throws Exception {
+        openSite();
+        checkCommonItem();
+    }
+    
+    /*
+    * UC016: Update segment
+    */
+    @Test
+    public void GUI_UC016() throws Exception {
         openSite();
         checkCommonItem();
     }

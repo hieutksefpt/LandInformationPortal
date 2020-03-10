a22454fdda8bf574600ce9b3d305e81dc6ae9b88
haonase06089 <40634213+haonase06089@users.noreply.github.com>
2020-03-10 15:15:19 +0700

    commit

---
+++ src/main/webapp/resources/js/listOwnRealEstate.js
@@ -0,0 +1,20 @@
+
+function initMap() {
+    //FPT 
+    var latitude = 21.012633;
+    var longitude = 105.527423;
+
+    var myLatLng = {lat: latitude, lng: longitude};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14,
+        mapTypeId: 'roadmap',
+        clickableIcons: false,
+        disableDoubleClickZoom: true,
+        fullscreenControl: false
+    });
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+}

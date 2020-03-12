00b5633a6592f0945fe7dc7e05659745297671a2
haonase06089 <40634213+haonase06089@users.noreply.github.com>
2020-03-12 23:20:43 +0700

    chưa tiến triển được gì cả =((((((((((

--- src/main/java/capstone/lip/landinformationportal/bean/ListOwnRealEstateBean.java
+++ src/main/java/capstone/lip/landinformationportal/bean/ListOwnRealEstateBean.java
@@ -5,11 +5,15 @@ import capstone.lip.landinformationportal.entity.LandsFeature;
 import capstone.lip.landinformationportal.entity.RealEstate;
 import capstone.lip.landinformationportal.service.Interface.IRealEstateService;
 import capstone.lip.landinformationportal.service.Interface.IUserService;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,29 +22,34 @@ import org.springframework.beans.factory.annotation.Autowired;
  *
  * @author AnhHao
  */
-
 @Named
 @ViewScoped
-public class ListOwnRealEstateBean implements Serializable{
-    
+public class ListOwnRealEstateBean implements Serializable {
+
     @Autowired
     private IRealEstateService realEstateService;
-    
+
     @Autowired
     private IUserService userService;
-    
+
     private List<RealEstate> listUserRealEstate;
     private RealEstate realEstateClicked;
-    
+
     @PostConstruct
     public void init() {
         listUserRealEstate = new ArrayList<>();
         listUserRealEstate = userService.getListRealEstate(1L);
     }
-    
-    public void clickOnButtonRowRealEstate(String realEstateId){
-        realEstateClicked = listUserRealEstate.stream().filter(x -> x.getRealEstateId().equals(Long.parseLong(realEstateId))).collect(Collectors.toList()).get(0);
-    }
+
+//    public void goToDetails(long userId) throws IOException {
+//        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+//        ec.redirect(ec.getRequestContextPath() + "/common/viewrealestatedetail?userId=" + userId);
+//    }
+//
+//    public String clickOnButtonRowRealEstate(String realEstateId) {
+//        realEstateClicked = listUserRealEstate.stream().filter(x -> x.getRealEstateId().equals(Long.parseLong(realEstateId))).collect(Collectors.toList()).get(0);
+//        return "common/viewrealestatedetail.xhtml?faces-redirect=true";
+//    }
 
     public List<RealEstate> getListUserRealEstate() {
         return listUserRealEstate;
@@ -50,8 +59,11 @@ public class ListOwnRealEstateBean implements Serializable{
         this.listUserRealEstate = listUserRealEstate;
     }
 
-    public ListOwnRealEstateBean(RealEstate realEstateClicked) {
+    public RealEstate getRealEstateClicked() {
+        return realEstateClicked;
+    }
+
+    public void setRealEstateClicked(RealEstate realEstateClicked) {
         this.realEstateClicked = realEstateClicked;
     }
-    
 }

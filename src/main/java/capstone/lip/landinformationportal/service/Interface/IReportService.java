f2128d1e77d8192ed075b5a8f449e3e6fa33e717
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-04-14 11:32:53 +0700

    add service and entity for report

---
+++ src/main/java/capstone/lip/landinformationportal/service/Interface/IReportService.java
@@ -0,0 +1,10 @@
+package capstone.lip.landinformationportal.service.Interface;
+
+import capstone.lip.landinformationportal.entity.Report;
+import capstone.lip.landinformationportal.entity.ReportId;
+
+public interface IReportService {
+	Report save(Report report);
+	
+	Report findById(Long userId, Long realEstateId);
+}
---
+++ src/main/java/capstone/lip/landinformationportal/service/ReportService.java
@@ -0,0 +1,37 @@
+package capstone.lip.landinformationportal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import capstone.lip.landinformationportal.entity.Report;
+import capstone.lip.landinformationportal.entity.ReportId;
+import capstone.lip.landinformationportal.repository.ReportRepository;
+import capstone.lip.landinformationportal.service.Interface.IReportService;
+
+@Service
+public class ReportService implements IReportService{
+
+	@Autowired
+	private ReportRepository reportRepository;
+	
+	@Override
+	public Report save(Report report) {
+		try {
+			return reportRepository.save(report);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public Report findById(Long userId, Long realEstateId) {
+		try {
+			return reportRepository.findByIdUserIdAndIdRealEstateId(userId, realEstateId);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}

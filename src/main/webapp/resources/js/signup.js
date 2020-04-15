c68be2a2d19850856fb38063058c690a4f7bdc25
hieutkse <hieutksefpt@gmail.com>
2020-04-15 09:37:41 +0700

    validate Sign Up + View Profile

--- src/main/webapp/resources/js/signup.js
+++ src/main/webapp/resources/js/signup.js
@@ -3,3 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+function showLogPassError(){
+    alert("Mật khẩu xác nhận không khớp ! ");
+}

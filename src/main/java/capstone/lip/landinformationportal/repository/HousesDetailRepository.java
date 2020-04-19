2e75c02a63cb4785ae06f0948b782c0a75a74bd1
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-04-19 14:12:55 +0700

    add find by composite id for houses detail and lands detail

--- src/main/java/capstone/lip/landinformationportal/repository/HousesDetailRepository.java
+++ src/main/java/capstone/lip/landinformationportal/repository/HousesDetailRepository.java
@@ -7,11 +7,13 @@ package capstone.lip.landinformationportal.repository;
 
 import capstone.lip.landinformationportal.entity.HousesDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Admin
  */
+@Repository
 public interface HousesDetailRepository extends JpaRepository<HousesDetail, Long>{
-    
+    HousesDetail findByIdHouseIdAndIdHousesFeatureId(Long houseId, Long housesFeatureId);
 }

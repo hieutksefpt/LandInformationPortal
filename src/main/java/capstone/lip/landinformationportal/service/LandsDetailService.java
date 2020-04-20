8a0eb01f84b239530af69f59ce038291db0365f2
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-04-20 21:16:39 +0700

    update service

--- src/main/java/capstone/lip/landinformationportal/service/LandsDetailService.java
+++ src/main/java/capstone/lip/landinformationportal/service/LandsDetailService.java
@@ -6,7 +6,9 @@
 package capstone.lip.landinformationportal.service;
 
 import capstone.lip.landinformationportal.entity.LandsDetail;
+import capstone.lip.landinformationportal.repository.LandRepository;
 import capstone.lip.landinformationportal.repository.LandsDetailRepository;
+import capstone.lip.landinformationportal.repository.LandsFeatureRepository;
 import capstone.lip.landinformationportal.service.Interface.ILandsDetailService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +24,21 @@ public class LandsDetailService implements ILandsDetailService {
     @Autowired
     private LandsDetailRepository landsDetailRepository;
 
+    @Autowired
+    private LandRepository landRepository;
+    
+    @Autowired
+    private LandsFeatureRepository landsFeatureRepository;
+    
     @Override
     public LandsDetail save(LandsDetail landsDetail) {
         try {
+        	if (!landRepository.existsById(landsDetail.getId().getLandId())) {
+        		throw new Exception("Land not found");
+        	}
+        	if (!landsFeatureRepository.existsById(landsDetail.getId().getLandsFeatureId())) {
+        		throw new Exception("Land feature not found");
+        	}
             return landsDetailRepository.save(landsDetail);
         } catch (Exception e) {
             e.printStackTrace();

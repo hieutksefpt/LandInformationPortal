dc4ed98f5075467752cda5a86cbe9503f83b7bf3
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-04-20 15:05:45 +0700

    fix name syntax composite key real estate adjacent segment repo

--- src/main/java/capstone/lip/landinformationportal/repository/RealEstateAdjacentSegmentRepository.java
+++ src/main/java/capstone/lip/landinformationportal/repository/RealEstateAdjacentSegmentRepository.java
@@ -15,5 +15,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface RealEstateAdjacentSegmentRepository extends JpaRepository<RealEstateAdjacentSegment, Long>{
-    RealEstateAdjacentSegment findByIdSegmentIdAndRealEstateId (Long segmentId, Long RealEstateId);
+    RealEstateAdjacentSegment findByIdSegmentOfStreetIdAndIdRealEstateId (Long segmentId, Long realEstateId);
 }

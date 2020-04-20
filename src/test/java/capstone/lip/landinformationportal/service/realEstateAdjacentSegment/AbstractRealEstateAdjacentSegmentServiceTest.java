2af880f61896d65fdf2764f6885d10c2d8d8be9e
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-04-20 10:50:26 +0700

    finish init test UT real estate adjacent segment service

---
+++ src/test/java/capstone/lip/landinformationportal/service/realEstateAdjacentSegment/AbstractRealEstateAdjacentSegmentServiceTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.lip.landinformationportal.service.realEstateAdjacentSegment;
+
+import capstone.lip.landinformationportal.common.CRUDTest;
+import capstone.lip.landinformationportal.entity.RealEstateAdjacentSegment;
+import capstone.lip.landinformationportal.entity.compositekey.RealEstateAdjacentSegmentId;
+import capstone.lip.landinformationportal.repository.RealEstateAdjacentSegmentRepository;
+import capstone.lip.landinformationportal.service.RealEstateAdjacentSegmentService;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Phong
+ */
+public abstract class AbstractRealEstateAdjacentSegmentServiceTest extends CRUDTest {
+    
+    @Autowired 
+    protected RealEstateAdjacentSegmentService instance;
+    
+    @Autowired
+    protected RealEstateAdjacentSegmentRepository repository;
+    
+    protected RealEstateAdjacentSegment sampleRealEstateAdjacentSegment = 
+            new RealEstateAdjacentSegment()
+                    .setId(setRealEstateAdjacentSegmentId(new RealEstateAdjacentSegmentId(),
+                            EXISTED_ID, EXISTED_ID));
+    
+    protected RealEstateAdjacentSegment setRealEstateAdjacentSegmentId(
+            RealEstateAdjacentSegment realEstateAdjacentSegment, 
+            Long segmentId, Long realEstateId) {
+        realEstateAdjacentSegment.setId(setRealEstateAdjacentSegmentId(
+                realEstateAdjacentSegment.getId(), segmentId, realEstateId));
+        
+        return realEstateAdjacentSegment;
+    }
+    
+    protected RealEstateAdjacentSegmentId setRealEstateAdjacentSegmentId(
+            RealEstateAdjacentSegmentId realEstateAdjacentSegmentId, 
+            Long segmentId, Long realEstateId) {
+        realEstateAdjacentSegmentId.setRealEstateId(realEstateId);
+        realEstateAdjacentSegmentId.setSegmentOfStreetId(segmentId);
+        
+        return realEstateAdjacentSegmentId;
+    }
+    
+    protected List<RealEstateAdjacentSegment> getListRealEstateAdjacentSegments() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        for (int i = 0; i < EXISTED_IDs.length; i++) {
+            details.add(repository.findByIdSegmentIdAndRealEstateId(EXISTED_ID, EXISTED_ID));
+        }
+        return details;
+    }
+    
+    protected void testInsertSuccess(RealEstateAdjacentSegment result, long records) {
+        //Update success
+        if (result != null) {
+            RealEstateAdjacentSegment actual = repository
+                    .findByIdSegmentIdAndRealEstateId(
+                            result.getId().getSegmentOfStreetId(),
+                            result.getId().getRealEstateId());
+            //Compare others
+            assertEquals(true, actual.equals(result));
+            //Test number of records is not changed
+            assertEquals(records + 1, repository.count());
+        } else {
+            fail();
+        }
+    }
+    
+    protected void testUpdateSuccess(RealEstateAdjacentSegment result, long records) {
+        //Update success
+        if (result != null) {
+            RealEstateAdjacentSegment actual = repository
+                    .findByIdSegmentIdAndRealEstateId(
+                            result.getId().getSegmentOfStreetId(),
+                            result.getId().getRealEstateId());
+            //Compare others
+            assertEquals(true, actual.equals(result));
+            //Test number of records is not changed
+            assertEquals(records, repository.count());
+        } else {
+            fail();
+        }
+    }
+
+    protected void testUpdateSuccess(List<RealEstateAdjacentSegment> results, long records) {
+        //Update success
+        if (results != null) {
+            for (RealEstateAdjacentSegment result : results) {
+                RealEstateAdjacentSegment actual = repository
+                    .findByIdSegmentIdAndRealEstateId(
+                            result.getId().getSegmentOfStreetId(),
+                            result.getId().getRealEstateId());
+            //Compare others
+            assertEquals(true, actual.equals(result));
+            }
+            //Test number of records is not changed
+            assertEquals(records, repository.count());
+        } else {
+            fail();
+        }
+    }
+    
+    protected void testDeleteSuccess(boolean result, 
+            List<RealEstateAdjacentSegment> details, long records) {
+        //Delete success
+        if (result) {
+            //Test exist in DB
+            for (RealEstateAdjacentSegment detail : details) {
+                assertEquals(true, repository
+                        .findByIdSegmentIdAndRealEstateId(
+                                detail.getId().getSegmentOfStreetId(), 
+                                detail.getId().getRealEstateId()) == null);
+            }
+            //Test number of records is decreased
+            assertEquals(records - details.size(), repository.count());
+        } else {
+            fail();
+        }
+    }
+}
---
+++ src/test/java/capstone/lip/landinformationportal/service/realEstateAdjacentSegment/RealEstateAdjacentSegmentServiceTest_2.java
@@ -0,0 +1,259 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.lip.landinformationportal.service.realEstateAdjacentSegment;
+
+import capstone.lip.landinformationportal.entity.RealEstateAdjacentSegment;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ *
+ * @author Phong
+ */
+@TestPropertySource(locations = "/application-test-data.properties")
+public class RealEstateAdjacentSegmentServiceTest_2 
+        extends AbstractRealEstateAdjacentSegmentServiceTest {
+    
+    /**
+     * @Description: Delete null list
+     * @Dependency: N/A
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_01() {
+        boolean result = instance.delete(null);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: List contain 1 real estate adjacent segment
+     * @Dependency: contain a non-existed real estate adjacent segment
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_02() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NOT_EXISTED_ID, NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: List contain 1 real estate adjacent segment
+     * @Dependency: contain an existed real estate adjacent segment
+     * @Expected Result: Success
+     */
+    @Test
+    public void FT_REASS_2_03() {
+        long records = repository.count();
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    EXISTED_ID, EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testDeleteSuccess(result, details, records);
+    }
+    
+    /**
+     * @Description: List contain many real estate adjacent segments
+     * @Dependency: contain a non-existed real estate adjacent segment
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_04() {
+        ArrayList<RealEstateAdjacentSegment> details = getListRealEstateAdjacentSegments();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NOT_EXISTED_ID, NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: List contain many real estate adjacent segment
+     * @Dependency: contain existed real estate adjacent segments
+     * @Expected Result: Success
+     */
+    @Test
+    public void FT_REASS_2_05() {
+        long records = repository.count();
+        ArrayList<RealEstateAdjacentSegment> details = getListRealEstateAdjacentSegments();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    EXISTED_ID, EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testDeleteSuccess(result, details, records);
+    }
+    
+    /**
+     * @Description: Delete negative real estate ID and negative segment ID
+     * @Dependency: N/A
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_06() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NEGATIVE_NOT_EXISTED_ID, NEGATIVE_NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete negative real estate ID and positive segment ID
+     * @Dependency: segment ID is not existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_07() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    POSITIVE_NOT_EXISTED_ID, NEGATIVE_NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete negative real estate ID and positive segment ID
+     * @Dependency: segment ID is existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_08() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    EXISTED_ID, NEGATIVE_NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and negative segment ID
+     * @Dependency: real estate ID is not existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_09() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NEGATIVE_NOT_EXISTED_ID, POSITIVE_NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and negative segment ID
+     * @Dependency: real estate ID is existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_10() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NEGATIVE_NOT_EXISTED_ID, EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and positive segment ID
+     * @Dependency: real estate ID and segment ID is existed
+     * @Expected Result: Success
+     */
+    @Test
+    public void FT_REASS_2_11() {
+        long records = repository.count();
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    EXISTED_ID, EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testDeleteSuccess(result, details, records);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and positive segment ID
+     * @Dependency: real estate ID and segment ID is not existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_12() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NOT_EXISTED_ID, NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and positive segment ID
+     * @Dependency: real estate ID is existed and segment ID is not existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_13() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    NOT_EXISTED_ID, EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete positive real estate ID and positive segment ID
+     * @Dependency: real estate ID is not existed and segment ID is existed
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_14() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    EXISTED_ID, NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete real estate ID and segment ID equal zero
+     * @Dependency: N/A
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_15() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(setRealEstateAdjacentSegmentId(sampleRealEstateAdjacentSegment, 
+                    ZERO_NOT_EXISTED_ID, ZERO_NOT_EXISTED_ID));
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+    
+    /**
+     * @Description: Delete null object
+     * @Dependency: N/A
+     * @Expected Result: Fail
+     */
+    @Test
+    public void FT_REASS_2_16() {
+        ArrayList<RealEstateAdjacentSegment> details = new ArrayList();
+        details.add(null);
+        boolean result = instance.delete(details);
+        
+        testFail(result);
+    }
+}

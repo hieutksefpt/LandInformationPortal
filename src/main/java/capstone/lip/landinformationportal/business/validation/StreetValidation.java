47f8dec0df3a22403f89a6101e25e22b3f9b68bb
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-05-06 09:29:58 +0700

    update street validate

--- src/main/java/capstone/lip/landinformationportal/business/validation/StreetValidation.java
+++ src/main/java/capstone/lip/landinformationportal/business/validation/StreetValidation.java
@@ -1,11 +1,13 @@
 package capstone.lip.landinformationportal.business.validation;
 
+import org.apache.commons.lang3.StringUtils;
+
 import capstone.lip.landinformationportal.common.constant.ValidateMessageCommon;
 import capstone.lip.landinformationportal.common.entity.Street;
 
 public class StreetValidation extends StringValidation{
 	public String isValidStreet(Street street) {
-		String error = isValidText(street.getStreetName());
+		String error = isValidName(street.getStreetName());
 		if (error.compareTo("") != 0) {
 			return error;
 		}
@@ -15,6 +17,21 @@ public class StreetValidation extends StringValidation{
 		}
 		if (street.getStreetLat() <= 0 || street.getStreetLng() <= 0) {
 			return ValidateMessageCommon.IS_NEGATIVE_NUMBER;
+		}
+		return "";
+	}
+	public String isValidName(String text) {
+		if (text == null) {
+			return ValidateMessageCommon.NULL;
+		}
+		if (text.trim().length() == 0) {
+			return ValidateMessageCommon.EMPTY;
+		}
+		if (text.matches(".*[^A-Za-z0-9\\p{L}\\p{M} ].*")) {
+			return ValidateMessageCommon.HAS_SPECIAL_CHAR;
+		}
+		if (StringUtils.isNumeric(text)) {
+			return ValidateMessageCommon.NUMERIC;
 		}
 		return "";
 	}

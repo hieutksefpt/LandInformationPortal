c86c317fa9db6a810595553fbbc21d4bd25573a3
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2020-05-06 21:50:22 +0700

    update contribute

--- src/main/java/capstone/lip/landinformationportal/business/bean/UpdateContributeRealEstateBean.java
+++ src/main/java/capstone/lip/landinformationportal/business/bean/UpdateContributeRealEstateBean.java
@@ -310,7 +310,7 @@ public class UpdateContributeRealEstateBean implements Serializable {
             if (newUploadRealEstate != null && realEstateAddress.isEmpty() && newLandName.isEmpty() && listLandFeatureValue.isEmpty() && newLandMoney.compareTo(BigDecimal.ZERO) == 0) {
                 PrimeFaces.current().executeScript("showLogEmptyLandHouse()");
                 variableSuccess = false;
-            } else if (newUploadRealEstate == null || !checkFillTextHouse()) {
+            } else if (newUploadRealEstate == null || !checkFillTextLand()) {
                 PrimeFaces.current().executeScript("showLogEmptyLandHouse()");
                 variableSuccess = false;
             } else if (newUploadRealEstate.getRealEstatePrice().compareTo(newLandMoney) == -1) {
@@ -333,7 +333,7 @@ public class UpdateContributeRealEstateBean implements Serializable {
             if (newUploadRealEstate != null && realEstateAddress.isEmpty() && newHouseName.isEmpty() && listHouseFeatureValue.isEmpty() && newHouseMoney.compareTo(BigDecimal.ZERO) == 0) {
                 PrimeFaces.current().executeScript("showLogEmptyLandHouse()");
                 variableSuccess = false;
-            } else if (newUploadRealEstate == null || !checkFillTextLand()) {
+            } else if (newUploadRealEstate == null || !checkFillTextHouse()) {
                 PrimeFaces.current().executeScript("showLogEmptyLandHouse()");
                 variableSuccess = false;
             }else if (StringUtils.isNumeric(newUploadRealEstate.getRealEstateName().toString()) || StringUtils.isNumeric(newHouseName.toString())

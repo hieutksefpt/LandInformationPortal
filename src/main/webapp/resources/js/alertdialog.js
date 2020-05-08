f7378e2d9c54b3b7df5db996a97657b92c80a376
Phong <trantuanphong.ttt@users.noreply.github.com>
2020-05-08 16:35:44 +0700

    remove test function abc

--- src/main/webapp/resources/js/alertdialog.js
+++ src/main/webapp/resources/js/alertdialog.js
@@ -3,13 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function abc() {
-    Swal.fire(
-            'Good job!',
-            'You clicked the button!',
-            'success'
-            );
-}
 function showError(content) {
     Swal.fire(
         'Opps!',
